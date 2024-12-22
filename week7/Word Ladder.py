1a9705fb8d92e3332cb5543a4f9dbc07377c1c48
LWhite200 <157498802+LWhite200@users.noreply.github.com>
2024-12-22 16:26:58 -0500

    Add files via upload

---
+++ week7/Word Ladder.py
@@ -0,0 +1,31 @@
+" Since we travel word to word, we can create a pseudo graph "
+" Then use bfs on that graph to find best route "
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+
+        nei = collections.defaultdict(list)
+        wordList.append(beginWord)
+        for word in wordList:
+            for j in range(len(word)):
+                pattern = word[:j] + "*" + word[j + 1 :]
+                nei[pattern].append(word)
+
+        visit = set([beginWord])
+        q = deque([beginWord])
+        res = 1
+        while q:
+            for i in range(len(q)):
+                word = q.popleft()
+                if word == endWord:
+                    return res
+                for j in range(len(word)):
+                    pattern = word[:j] + "*" + word[j + 1 :]
+                    for neiWord in nei[pattern]:
+                        if neiWord not in visit:
+                            visit.add(neiWord)
+                            q.append(neiWord)
+            res += 1
+        return 0
