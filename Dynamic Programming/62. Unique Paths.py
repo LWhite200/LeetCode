6355042f5b4f12947ca27da53b6e92f2842b3cf9
LWhite200 <157498802+LWhite200@users.noreply.github.com>
2025-01-21 20:55:40 -0500

    Add files via upload

---
+++ Dynamic Programming/62. Unique Paths.py
@@ -0,0 +1,12 @@
+" Fibonachii sequence "
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        DP = [1] * n
+        
+        for i in range(m - 1):
+            for j in range(n - 2, -1, -1):
+                DP[j] = DP[j] + DP[j + 1]
+                    
+        return DP[0]
+    
