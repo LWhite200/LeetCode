3189e8dd503ec8b7abe3ebeaa209cca525b6d95e
LWhite200 <157498802+LWhite200@users.noreply.github.com>
2025-01-24 19:47:51 -0500

    Add files via upload

---
+++ Dynamic Programming/85. Maximum Rectangle.py
@@ -0,0 +1,42 @@
+" We use the solution in largest rectangle in hostogram "
+" We traverse the 2D arraay and we store the current maximum height of each column "
+" Proud I came up with this on my own "
+
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        rows, cols = len(matrix), len(matrix[0])
+        maxRect = 0
+        dp = [0] * cols
+
+        def LRH(heights: List[int]):
+            n = len(heights)
+            stack = []
+            idx = []
+            maxArea = 0
+            for i, h in enumerate(heights):
+                heightStart = i
+                while stack and stack[-1] > h:
+                    h2 = stack.pop()
+                    i2 = idx.pop()
+                    maxArea = max(maxArea, h2 * (i - i2)) 
+                    heightStart = i2  
+                
+                stack.append(h)
+                idx.append(heightStart)
+            for h in stack:
+                i = idx.pop(0)
+                maxArea = max(maxArea, h * (n - i)) 
+            return maxArea
+        # ---------------------------------------------------
+
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == "1":
+                    dp[j] += 1
+                else:
+                    dp[j] = 0
+
+            maxRect = max(maxRect, LRH(dp))
+
+        
+        return maxRect
