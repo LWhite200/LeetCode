57a7b6fc8dee7a96c5acbcc8a0f52912ca6fd4d1
LWhite200 <157498802+LWhite200@users.noreply.github.com>
2025-01-28 17:16:24 -0500

    Add files via upload

---
+++ Dynamic Programming/96. Unique Binary Search Trees.py
@@ -0,0 +1,16 @@
+" We will go up from zero to n to see the possible amount of combinations each (tree of _ nodes) can have "
+" this is done by calculate the  left and right number of combinations "
+
+class Solution:
+    def numTrees(self, n: int) -> int:
+        dp = [1] * (n + 1)
+        
+        for nodes in range(2, n + 1):
+            total = 0
+            for root in range(1, nodes + 1):
+                left = root - 1
+                right = nodes - root
+                total += dp[left] * dp[right]
+            dp[nodes] = total
+        
+        return dp[n]
