c5f63f41945f81019133be4f846c0bb4f5cc01bf
LWhite200 <157498802+LWhite200@users.noreply.github.com>
2025-02-05 20:48:53 -0500

    Add files via upload

--- Dynamic Programming/96. Unique Binary Search Trees.py
+++ Dynamic Programming/96. Unique Binary Search Trees.py
@@ -1,13 +1,15 @@
+" Triangles chanhge this "
+
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        res = [[1]]
+        rs = [[1]]
 
         for _ in range(numRows - 1):
-            dummy_row = [0] + res[-1] + [0]
+            dummy_row = [0] + rs[-1] + [0]
             row = []
 
-            for i in range(len(res[-1]) + 1):
+            for i in range(len(rs[-1]) + 1):
                 row.append(dummy_row[i] + dummy_row[i+1])
-            res.append(row)
+            rs.append(row)
         
-        return res
+        return rs
